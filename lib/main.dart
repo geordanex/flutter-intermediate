eac0b296ab4c227876592b995128e4f068cf4a54
Geordan Sánchez <geordan.sanchez@entelgy.com>
2019-03-18 11:10:19 -0500

    Adding tooltip

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyHomePage());
+void main() => runApp(new MaterialApp(
+      home: new MyHomePage(),
+    ));
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -8,23 +10,30 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String _value = 'Nothing yet';
+
+  void _onPressed() => setState(() => _value = new DateTime.now().toString());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Name here'),
-      ),
-      body: new Container(
-        padding: new EdgeInsets.all(32.0),
-        child: new Center(
-          child: new Column(
-            children: <Widget>[
-              new Text('Add Widget here')
-            ],
-          ),
+        appBar: AppBar(
+          title: Text('Name here'),
         ),
-      )
-    );
+        body: new Container(
+          padding: new EdgeInsets.all(32.0),
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                new Text(_value),
+                new IconButton(
+                  icon: new Icon(Icons.timer),
+                  onPressed: _onPressed,
+                  tooltip: 'Click me',
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
